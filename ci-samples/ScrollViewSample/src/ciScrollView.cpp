3f1ca740334f002f89409d549e1e6ec30fc7cf0f
Rene Christen <accounts@renechristen.net>
2016-01-28 13:37:02 +1100

    ciScrollView

---
+++ ci-samples/ScrollViewSample/src/ciScrollView.cpp
@@ -0,0 +1,76 @@
+//
+//	    ┌─┐╔═╗┌┬┐┌─┐
+//      │  ║ ║ ││├┤
+//      └─┘╚═╝─┴┘└─┘
+//	 ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+//	 │  ├─┤║║║└┐┌┘├─┤└─┐
+//	 └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+//	http://CodeOnCanvas.cc
+//
+// Created by Rene Christen on 28/01/2016.
+// Copyright (c) 2016, Code on Canvas Pty Ltd
+//
+
+#ifdef COC_CI
+
+#include "ciScrollView.h"
+
+namespace coc {
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+ciScrollView::ciScrollView() : ScrollView() {
+#ifdef CINDER_COCOA_TOUCH
+	bPinchZoomSupported = false;
+#endif
+}
+
+//----------------------------------------------------------
+void ciScrollView::begin() {
+	gl::pushModelMatrix();
+	gl::multModelMatrix(getMatrix());
+}
+
+void ciScrollView::end() {
+	gl::popModelMatrix();
+}
+
+//----------------------------------------------------------
+void ciScrollView::mouseMoved(MouseEvent & mouse) {
+	ScrollView::mouseMoved(mouse.getPos().x, mouse.getPos().y);
+}
+static int mouseButton = 0;//for consistency with OF style functions in case class only
+void ciScrollView::mouseDragged(MouseEvent & mouse) {
+	ScrollView::mouseDragged(mouse.getPos().x, mouse.getPos().y, mouseButton);
+}
+
+void ciScrollView::mousePressed(MouseEvent & mouse) {
+	ScrollView::mousePressed(mouse.getPos().x, mouse.getPos().y, mouseButton);
+}
+
+void ciScrollView::mouseReleased(MouseEvent & mouse) {
+	ScrollView::mouseReleased(mouse.getPos().x, mouse.getPos().y, mouseButton);
+}
+
+
+//----------------------------------------------------------
+void ciScrollView::setUserInteractionOn() {
+
+	cbMouseDown = getWindow()->getSignalMouseDown().connect( std::bind( &ciScrollView::mousePressed, this, std::placeholders::_1 ) );
+	cbMouseDrag = getWindow()->getSignalMouseDrag().connect( std::bind( &ciScrollView::mouseDragged, this, std::placeholders::_1 ) );
+	cbMouseUp = getWindow()->getSignalMouseUp().connect( std::bind( &ciScrollView::mouseReleased, this, std::placeholders::_1 ) );
+}
+
+void ciScrollView::setUserInteractionOff() {
+
+	cbMouseDown.disconnect();
+	cbMouseDrag.disconnect();
+	cbMouseUp.disconnect();
+
+}
+
+}//namespace coc
+
+#endif
---
+++ ci-samples/ScrollViewSample/src/ciScrollView.h
@@ -0,0 +1,54 @@
+//
+//	    ┌─┐╔═╗┌┬┐┌─┐
+//      │  ║ ║ ││├┤
+//      └─┘╚═╝─┴┘└─┘
+//	 ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+//	 │  ├─┤║║║└┐┌┘├─┤└─┐
+//	 └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+//	http://CodeOnCanvas.cc
+//
+// Created by Rene Christen on 28/01/2016.
+// Copyright (c) 2016, Code on Canvas Pty Ltd
+//
+
+#pragma once
+
+#ifdef COC_CI
+
+#include "cocScrollView.h"
+#include "cinder/app/App.h"
+
+namespace coc {
+
+class ciScrollView : public coc::ScrollView {
+
+public:
+
+	ciScrollView();
+
+	void begin();
+	void end();
+
+	void mouseMoved(ci::app::MouseEvent & mouse);
+	void mouseDragged(ci::app::MouseEvent & mouse);
+	void mousePressed(ci::app::MouseEvent & mouse);
+	void mouseReleased(ci::app::MouseEvent & mouse);
+
+#ifdef CINDER_COCOA_TOUCH
+	//todo: touch events
+#endif
+
+protected:
+
+	void setUserInteractionOn();
+	void setUserInteractionOff();
+
+	ci::signals::ScopedConnection  cbMouseDown, cbMouseDrag, cbMouseUp;
+
+};//class ciScrollView
+
+}//namespace coc
+
+#endif
+
+
